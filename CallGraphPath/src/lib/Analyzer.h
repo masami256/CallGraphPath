4cd9716985b093e193cbf202a7c71422f642cd59
Masami Ichikawa <masami256@gmail.com>
2025-04-07 00:07:11 +0900

    update
    Signed-off-by: Masami Ichikawa <masami256@gmail.com>

--- CallGraphPath/src/lib/Analyzer.h
+++ CallGraphPath/src/lib/Analyzer.h
@@ -4,11 +4,4 @@
 #include <set>
 
 typedef std::vector< std::pair<llvm::Module*, llvm::StringRef> > ModuleList;
-// Mapping module to its file name.
-typedef std::unordered_map<llvm::Module*, llvm::StringRef> ModuleNameMap;
-// The set of strings.
-typedef std::set<std::string> StrSet;
-typedef llvm::SmallPtrSet<llvm::Function*, 8> FuncSet;
-
-using namespace llvm;
 
